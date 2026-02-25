2a8c01813d31b7f6fa113fd3d48be2c3f6f048f5
Chris Blanco <chrisblanco.2004@gmail.com>
2026-02-25 12:15:26 -0600

    Enhance MenuBar component with improved styles and functionality; integrate CV download feature

---
+++ src/Menu-bar/CV-Download-Button/CVdownloadButton.jsx
@@ -0,0 +1,12 @@
+import './CVdownloadButton.css'
+
+function CVdownloadButton() {
+    return (
+        <div id='downloadCVbutton-container' >
+            <img src='./src/assets/download.png'></img>
+            <button id='downloadCVbutton'>Download CV</button>
+        </div>
+    )
+}
+
+export default CVdownloadButton
